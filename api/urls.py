bb02454785e9a27fb877363c9c20a2f2275b9460
araviindm <araviindm@gmail.com>
2024-03-13 12:32:57 +0530

    generate_plot API added

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import battery_cell, get_battery_cell_by_id
+from .views import battery_cell, get_battery_cell_by_id, generate_plot
 
 urlpatterns = [
     path('batterycell', battery_cell, name='batterycell'),
     path('batterycell/<str:id>', get_battery_cell_by_id,
          name='batterycell-detail'),
+    path('impedance/plot', generate_plot, name='generate_plot'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
+import base64
+import io
+import os
+import matplotlib.pyplot as plt
+import numpy as np
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import api_view, parser_classes
@@ -5,6 +10,11 @@ from rest_framework.response import Response
 
 from api.models import BatteryCell
 from .serializers import BatteryCellSerializer
+
+import pandas as pd
+from impedance.visualization import plot_bode
+import matplotlib
+matplotlib.use('Agg')
 
 
 @api_view(['GET', 'POST'])
@@ -23,7 +33,7 @@ def battery_cell(request, format=None):
             serializer = BatteryCellSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.data, status=status.HTTP_200_OK)
             print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
@@ -39,3 +49,39 @@ def get_battery_cell_by_id(request, id, format=None):
 
     serializer = BatteryCellSerializer(battery_cell)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@parser_classes([MultiPartParser, FormParser])
+def generate_plot(request):
+    if 'file' not in request.FILES:
+        return Response({'error': 'File not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+    file = request.FILES['file']
+
+    try:
+        df = pd.read_csv(file, sep='\t', header=None)
+        df = df[0].str.split(',', expand=True).astype(float)
+        frequency = df.iloc[:, 0].values
+        impedance_real = df.iloc[:, 1].values
+        impedance_imag = df.iloc[:, 2].values
+
+        impedance_complex = impedance_real + 1j * impedance_imag
+
+        plt.rcParams['figure.figsize'] = (10, 10)
+        plot_bode(frequency, impedance_complex)
+
+        plt.xlabel('Frequency')
+        plt.ylabel('Impedance')
+
+        plt.savefig('bode_plot.png')
+        plt.close()
+
+        with open('bode_plot.png', 'rb') as image_file:
+            plot_image_base64 = base64.b64encode(
+                image_file.read()).decode('utf-8')
+        os.remove('bode_plot.png')
+        return Response({'plot': plot_image_base64}, status=status.HTTP_200_OK)
+    except Exception as e:
+
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
